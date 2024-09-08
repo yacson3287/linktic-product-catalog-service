91b488cfa3bd05ec7b5c503c446d844491d5cb7f
yacson3287 <yacson.ramirez@gmail.com>
2024-09-07 23:12:42 -0400

    first commit

---
+++ src/main/java/com/linktic/product_catalog_service/ProductCatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.linktic.product_catalog_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductCatalogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductCatalogServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/linktic/product_catalog_service/ProductCatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linktic.product_catalog_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
