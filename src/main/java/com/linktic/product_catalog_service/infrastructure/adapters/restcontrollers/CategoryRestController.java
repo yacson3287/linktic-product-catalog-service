990283af03b21a6d4abe6a2acb4ffeefff004115
yacson3287 <yacson.ramirez@gmail.com>
2024-09-11 02:29:40 -0400

    add swagger configurations

--- src/main/java/com/linktic/product_catalog_service/infrastructure/adapters/restcontrollers/CategoryRestController.java
+++ src/main/java/com/linktic/product_catalog_service/infrastructure/adapters/restcontrollers/CategoryRestController.java
@@ -6,6 +6,8 @@ import com.linktic.product_catalog_service.domain.ports.in.UpdateCategoryUseCase
 import com.linktic.product_catalog_service.infrastructure.adapters.restcontrollers.dto.CategoryCreateRequest;
 import com.linktic.product_catalog_service.infrastructure.adapters.restcontrollers.dto.CategoryResponse;
 import com.linktic.product_catalog_service.infrastructure.adapters.restcontrollers.dto.CategoryUpdateRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -16,12 +18,15 @@ import java.util.List;
 @RestController
 @RequestMapping("category")
 @RequiredArgsConstructor
+@Tag(name = "Category", description = "Catalog Categories management")
 public class CategoryRestController {
 
     private final CreateCategoryUseCase createCategoryUseCase;
     private final FindCategoryUseCase findCategoryUseCase;
     private final UpdateCategoryUseCase updateCategoryUseCase;
 
+
+    @Operation(summary = "Add a new category to catalog")
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public CategoryResponse create(@RequestBody @Valid CategoryCreateRequest request) {
@@ -29,22 +34,25 @@ public class CategoryRestController {
         return CategoryResponse.convertFromDomain(category);
     }
 
+    @Operation(summary = "Find a category of catalog")
     @GetMapping("{id}")
     public CategoryResponse findById(@PathVariable Long id) {
         var category = findCategoryUseCase.execute(id);
         return CategoryResponse.convertFromDomain(category);
     }
 
+    @Operation(summary = "Find all categories of catalog")
     @GetMapping
-    public List<CategoryResponse> findById() {
+    public List<CategoryResponse> findAll() {
         var categories = findCategoryUseCase.execute();
         return categories.stream()
                 .map(CategoryResponse::convertFromDomain)
                 .toList();
     }
 
+    @Operation(summary = "update a category of catalog")
     @PutMapping
-    public CategoryResponse update(@RequestBody CategoryUpdateRequest request) {
+    public CategoryResponse update(@RequestBody @Valid CategoryUpdateRequest request) {
         var category = updateCategoryUseCase.execute(request.convertToDomain());
         return CategoryResponse.convertFromDomain(category);
     }
--- src/main/java/com/linktic/product_catalog_service/infrastructure/adapters/restcontrollers/ProductRestController.java
+++ src/main/java/com/linktic/product_catalog_service/infrastructure/adapters/restcontrollers/ProductRestController.java
@@ -8,6 +8,8 @@ import com.linktic.product_catalog_service.domain.ports.in.SubTractProductUseCas
 import com.linktic.product_catalog_service.infrastructure.adapters.restcontrollers.dto.ProductCreateRequest;
 import com.linktic.product_catalog_service.infrastructure.adapters.restcontrollers.dto.ProductResponse;
 import com.linktic.product_catalog_service.infrastructure.adapters.restcontrollers.dto.UpdateProductQuantityRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +19,7 @@ import java.util.List;
 @RestController
 @RequestMapping("product")
 @RequiredArgsConstructor
+@Tag(name = "Product", description = "Catalog products management")
 public class ProductRestController {
 
     private final CreateProductUseCase createProductUseCase;
@@ -25,18 +28,21 @@ public class ProductRestController {
     private final SubTractProductUseCase subTractProductUseCase;
 
 
+    @Operation(summary = "Add a new product to catalog")
     @PostMapping
     public ProductResponse create(@RequestBody @Valid ProductCreateRequest request) {
         var product = createProductUseCase.execute(request.convertToDomain());
         return ProductResponse.convertFromDomain(product);
     }
 
+    @Operation(summary = "find a product of catalog")
     @GetMapping("{id}")
     public ProductResponse findById(@PathVariable Long id) {
         var product = findProductUseCase.execute(id);
         return ProductResponse.convertFromDomain(product);
     }
 
+    @Operation(summary = "Find products of a category")
     @GetMapping("/category/{categoryId}")
     public List<ProductResponse> findByCategory(@PathVariable Long categoryId) {
 
@@ -50,6 +56,7 @@ public class ProductRestController {
                 .toList();
     }
 
+    @Operation(summary = "Find all products of catalog")
     @GetMapping
     public List<ProductResponse> findAll() {
         var products = findProductUseCase.execute();
@@ -58,6 +65,7 @@ public class ProductRestController {
                 .toList();
     }
 
+    @Operation(summary = "Increase quantity of a product")
     @PutMapping("add-quantity")
     public List<ProductResponse> addProductQuantity(@RequestBody List<UpdateProductQuantityRequest> requests) {
         var products = addProductsUseCase.execute(requests.stream().map(UpdateProductQuantityRequest::convertToDomain).toList());
@@ -66,12 +74,14 @@ public class ProductRestController {
                 .toList();
     }
 
+    @Operation(summary = "Subtract quantity of a product")
     @PutMapping("subtract-quantity")
     public ProductResponse subtractProductQuantity(@RequestBody @Valid UpdateProductQuantityRequest request) {
         var product = subTractProductUseCase.execute(request.convertToDomain());
         return ProductResponse.convertFromDomain(product);
     }
 
+    @Operation(summary = "Find product by a id list")
     @PostMapping("find-by-ids")
     public List<ProductResponse> findByIds(@RequestBody List<Long> ids) {
         var products = findProductUseCase.execute(ids);
